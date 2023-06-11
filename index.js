4f26738143e7e9d8847d234e71d41a4bdb0e8b8d
Parker Jones <elder.jones888@gmail.com>
2023-06-10 18:40:15 -0600

    OAuth

--- index.js
+++ index.js
@@ -93,20 +93,3 @@ async function listConnectionNames(auth) {
 }
 
 authorize().then(listConnectionNames).catch(console.error);
-
-const express = require('express');
-const router = express.Router();
-
-const moviesController = require('../controllers/movies');
-
-router.get('/', moviesController.getAll);
-
-router.get('/:id', moviesController.getSingle);
-
-router.post('/', moviesController.createEntry);
-
-router.put('/:id', moviesController.updateEntry);
-
-router.delete('/:id', moviesController.deleteEntry);
-
-module.exports = router;
